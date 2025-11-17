1d6a93fe17512b4a05be0eed0ac1c83dd9e9c49a
AliasgharHeidari <Aliasgharaliali1213@gmail.com>
2025-11-17 23:36:13 +0330

    feat : service oprations updated for endpoints

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/AliasgharHeidari/mobile-numbers-v1/internal/model"
 	onmemory "github.com/AliasgharHeidari/mobile-numbers-v1/internal/repository/on-memory"
 )
@@ -16,4 +18,52 @@ func GetUserByID(id int) (model.User, error) {
 		}
 	}
 	return model.User{}, nil
+}
+
+func CreateUser(user model.User) (int, error) {
+	user.ID = len(onmemory.Users)+ 1
+
+	onmemory.Users = append(onmemory.Users, user)
+	return user.ID, nil
+}
+
+func UpdateUserByID(id int, UpdatedUser model.User) error {
+	for i := range onmemory.Users {
+		if onmemory.Users[i].ID == id {
+		onmemory.Users[i] = UpdatedUser		
+		onmemory.Users[i].ID = id
+		return nil
+		}
+	}
+	return fmt.Errorf("user not found")
+}
+
+func DeleteUserByID(id int) error{
+	for i := range onmemory.Users {
+		if onmemory.Users[i].ID == id {
+			onmemory.Users = append(onmemory.Users[:i], onmemory.Users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("user not found")
+}
+
+func AddMobileNumber(id int, mobileNumber model.MobileNumber) error {
+	for i := range onmemory.Users{
+		if onmemory.Users[i].ID == id{
+			onmemory.Users[i].MobileNumbers = append(onmemory.Users[i].MobileNumbers, mobileNumber)
+			return nil
+		}
+	}
+	return fmt.Errorf("user not found")
+}
+
+func DeleteMobileNumber(id int) error {
+	for i := range onmemory.Users{
+		if onmemory.Users[i].ID == id {
+			onmemory.Users[i].MobileNumbers = nil
+			return nil
+		}
+	}
+	return fmt.Errorf("user not found")
 }
